d3cdef65e8945dd34317a679d4b9a5bcfc05716c
Elionora01 <elinabakirova15749@gmail.com>
2024-01-13 20:18:55 +0500

    2

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать 
 на старте выполнения алгоритма. При решении не рекомендуется пользоваться
 коллекциями, лучше обойтись исключительно массивами. */
+
+
+Console.Write("Введите количество строк в массиве: ");
+int n = int.Parse(Console.ReadLine());
+string[] array = new string[n]; 
+for (int i = 0; i < n; i++)     
+{
+    Console.Write($"Введите строку {i + 1}: ");
+    array[i] = Console.ReadLine();
+}
+string[] newArray = GetShortStrings(array);
